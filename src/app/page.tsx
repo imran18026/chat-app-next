26737a0f975489b9e9849bab289e42842df72298
imran18026 <mdimranhossain.ist@gmail.com>
2024-10-09 18:49:15 +0600

    clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import { Button, Flex } from "antd";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1>Test</h1>
-      </div>
-      <div>
-        <Button type="primary">Click me</Button>
-      </div>
+    <div className="p-10">
+      <UserButton />
     </div>
   );
 }
