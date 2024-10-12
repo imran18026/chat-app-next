e6f91eff658e252689b268822aac408fd327b165
imran18026 <mdimranhossain.ist@gmail.com>
2024-10-12 14:57:19 +0600

    auth done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const loggedInUserData = await currentUser();
   return (
     <div className="p-10">
-      <UserButton />
+      <div className="flex flex-col gap-3 text-3xl">
+        <UserButton afterSwitchSessionUrl="/sign-in" />
+        <span>First name:{loggedInUserData?.firstName} </span>
+        <span>Last name: {loggedInUserData?.lastName}</span>
+        <span>User name: {loggedInUserData?.username} </span>
+        <span>Email: {loggedInUserData?.emailAddresses[0]?.emailAddress}</span>
+      </div>
     </div>
   );
 }
